17d836e95e5bab2a1d8ad9b2b9c8a813754848cd
Henning Ottesen <henning.ottesen@tietoevry.com>
2023-02-22 14:22:49 +0100

    Add widget API

---
+++ src/decs.d.ts
@@ -0,0 +1,1 @@
+declare module "@puzzel/widget-api-lib"
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log("Hello world!!");
+import * as WidgetApi from '@puzzel/widget-api-lib';
+
+WidgetApi.connect().then(
+    () => console.log('connected'),
+    (error: any) => console.error('Failed to connect', error),
+)
