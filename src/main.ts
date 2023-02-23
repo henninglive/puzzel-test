de0abd383cecac62d4c5cba6574ab5c0db9addc6
Henning Ottesen <henning.ottesen@tietoevry.com>
2023-02-23 07:57:07 +0100

    Add textarea console

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
 import {connect, Api} from "@puzzel/widget-api-lib";
 
-connect().then(
-    (api) => OnConnected(api),
-    (error: any) => console.error('Failed to connect Puzzel agent', error),
-)
+function logTextArea(msg: string) {
+    if (!textarea || !(textarea instanceof HTMLTextAreaElement)) {
+        return;
+    }
+
+    textarea.textContent += msg + "\n";
+    textarea.scrollTop = textarea.scrollHeight;
+}
 
 async function OnConnected(api: Api) {
-    console.log('Connected')
-    
-    api.on('SYSTEM_CALL_STATE_CHANGE', onCallStateChange)
+    logTextArea('Connected');
+
     api.on('SYSTEM_USER_STATUS_CHANGED', onUserStatusChanged)
+    api.on('SYSTEM_CALL_STATE_CHANGE', onCallStateChange)
+    api.on('SYSTEM_STATE_UPDATED', onUserStatusUpdated)
+    
     api.ready();
 
-    console.log('Ready')
+    logTextArea('Ready');
 }
 
-function onCallStateChange(arg1: any, arg2: any) {
-    console.log('onCallStateChange', arg1, arg2);
+function onUserStatusChanged(arg: any) {
+    logTextArea(`onUserStatusChanged: ${JSON.stringify(arg)}`);
 }
 
-function onUserStatusChanged(arg1: any) {
-    console.log('onUserStatusChanged', arg1);
+function onCallStateChange(arg: any) {
+    logTextArea(`onCallStateChange: ${JSON.stringify(arg)}`);
 }
+
+function onUserStatusUpdated(arg: any) {
+    logTextArea(`onUserStatusUpdated: ${JSON.stringify(arg)}`);
+}
+
+
+const textarea = document.querySelector("textarea.console");
+
+connect().then(
+    (api) => OnConnected(api),
+    (error: any) => logTextArea('Failed to connect Puzzel agent: ' + error),
+)
